d23ea24968240bbfebbdc560bef922ef3afe5451
jbowdle <johnwbowdle@gmail.com>
2023-05-03 13:20:45 -0500

    Created model associations, imported/synced sequelize in server.js, created get routes for category

--- models/index.js
+++ models/index.js
@@ -4,13 +4,27 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
 
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+  },
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: {
+    model: ProductTag,
+  },
+});
 
 module.exports = {
   Product,
