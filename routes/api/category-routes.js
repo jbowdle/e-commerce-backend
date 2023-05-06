ef49bb62f5807379d8f49053d92135069eec8173
jbowdle <johnwbowdle@gmail.com>
2023-05-05 22:12:40 -0500

    category post, put, and delete routes now functional

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,16 +37,54 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const newCategory = await Category.create(req.body);
+    res.status(200).json(newCategory);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryUpdate = await Category.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryUpdate) {
+      res.status(404).json({ message: 'Provided ID does not match any categories'});
+      return;
+    }
+
+    res.status(200).json(categoryUpdate);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryTarget = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryTarget) {
+      res.status(404).json({ message: 'Provided ID does not match any categories'});
+      return;
+    }
+    
+    res.status(200).json(categoryTarget);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
